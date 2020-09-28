ba085b4274402d3e7cdd297306cae70dfa4153fc
Petikov Vladimir <vx7xv@inbox.ru>
2020-09-28 21:44:16 +0300

    Закончена верстка десятого блока

--- js/main.js
+++ js/main.js
@@ -24,4 +24,28 @@ $( document ).ready(function() {
         }
     })
 
-});
+    $('#slider').owlCarousel({
+        center: true,
+        loop:true,
+        margin:15,
+        nav:true,
+         autoplay:true,
+         autoplayTimeout:1000,
+        dots: true,
+        navText: [
+        '<svg width="35" height="35" class="fff" stroke="red" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
+        '<svg width="35" height="35" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+   });
